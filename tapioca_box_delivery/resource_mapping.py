33714b78066f0a7f2ee3928ae3f6bb45a5df25b4
Wilson <wilsontads@gmail.com>
2021-08-30 14:07:29 -0300

    add server error test

--- tapioca_box_delivery/resource_mapping.py
+++ tapioca_box_delivery/resource_mapping.py
@@ -11,4 +11,19 @@ RESOURCE_MAPPING = {
         "docs": "https://dev.api.boxdelivery.com.br/doc/index.html#api-Order-PostOrders",
         "methods": ["POST"],
     },
+    "cancel_order": {
+        "resource": "orders/:id/cancel",
+        "docs": "https://dev.api.boxdelivery.com.br/doc/index.html#api-Order-PutOrdersIdCancel",
+        "methods": ["PUT"],
+    },
+    "detail_order": {
+        "resource": "orders/:id",
+        "docs": "https://dev.api.boxdelivery.com.br/doc/index.html#api-Order-GetOrdersId",
+        "methods": ["GET"],
+    },
+    "end_order": {
+        "resource": "orders/:id/finish",
+        "docs": "https://dev.api.boxdelivery.com.br/doc/index.html#api-Order-PutOrdersIdFinish",
+        "methods": ["PUT"],
+    },
 }
--- tests/test_client.py
+++ tests/test_client.py
@@ -18,10 +18,8 @@ def test_invalid_token_client(box_delivery_url, box_delivery_client):
 def test_server_error(box_delivery_url, box_delivery_client):
     endpoint = f"{box_delivery_url}orders/calculate-value/4/-16.731470/-49.283660"
     responses.add(responses.GET, endpoint, status=500)
-
     with pytest.raises(ServerError) as server_error:
         box_delivery_client.calculate_value(
             vehicle_type=4, destination_lat="-16.731470", destination_lng="-49.283660"
         ).get()
-
     assert "500" in str(server_error.value)
