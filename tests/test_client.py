d578f726ca6689b7686339dc94f7ecc9cf25c064
Wilson <wilsontads@gmail.com>
2021-08-30 14:01:00 -0300

    add server error test

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,6 @@
 import pytest
 import responses
-from tapioca.exceptions import ClientError
+from tapioca.exceptions import ClientError, ServerError
 
 
 @responses.activate
@@ -12,3 +12,16 @@ def test_invalid_token_client(box_delivery_url, box_delivery_client):
             vehicle_type=4, destination_lat="-16.731470", destination_lng="-49.283660"
         ).get()
     assert "401" in str(client_error.value)
+
+
+@responses.activate
+def test_server_error(box_delivery_url, box_delivery_client):
+    endpoint = f"{box_delivery_url}orders/calculate-value/4/-16.731470/-49.283660"
+    responses.add(responses.GET, endpoint, status=500)
+
+    with pytest.raises(ServerError) as server_error:
+        box_delivery_client.calculate_value(
+            vehicle_type=4, destination_lat="-16.731470", destination_lng="-49.283660"
+        ).get()
+
+    assert "500" in str(server_error.value)
